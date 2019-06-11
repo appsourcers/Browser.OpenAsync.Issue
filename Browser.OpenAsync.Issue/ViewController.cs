cefa60b5da696bd2ea5c020e7e9bb1c84478168c
Aleksandar Milic <appsourcers@gmail.com>
2019-06-11 13:52:57 +0200

    Initial commit

---
+++ Browser.OpenAsync.Issue/ViewController.cs
@@ -0,0 +1,36 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using Xamarin.Essentials;
+
+namespace Browser.OpenAsync.Issue
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            clickMe.TouchUpInside += ClickMe_TouchUpInside;
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        void ClickMe_TouchUpInside(object sender, EventArgs e)
+        {
+            Xamarin.Essentials.Browser.OpenAsync("https://github.com/xamarin/Essentials/blob/master/SUBMITTING-ISSUES.md#ios-specific-concerns", new BrowserLaunchOptions
+            {
+                TitleMode = BrowserTitleMode.Show,
+                LaunchMode = BrowserLaunchMode.External
+            });
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
